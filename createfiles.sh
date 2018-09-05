679f6239041960b14225d23631badaa15c5f914f
rahapirzadeh <36495199+rahapirzadeh@users.noreply.github.com>
2018-09-05 14:23:24 -0700

    Add files via upload

--- createfiles.sh
+++ createfiles.sh
@@ -1,7 +1,6 @@
-#$/bin/bash
-
+#!/bin/bash
 mkdir tmpfiles
 cd tmpfiles
-for i in $(seq 100); do mkdir "tmp$i.tmp";done
+for i in $(seq -w 100); do echo > tmp$i.tmp; done
+for i in $(seq -w 100); do echo "Temporary file $i" >> tmp$i.tmp; done
 cd -
-exit 0
